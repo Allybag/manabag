aff6cbb37961253cb479b4c297a8caa9b25c8131
Ally <bagkingally@gmail.com>
2024-09-30 16:35:13 -0400

    Implement adding two matrices and multiplying by a scalar

--- src/matrix.rs
+++ src/matrix.rs
@@ -8,13 +8,13 @@ impl <T: Add<Output = T> + Mul<Output = T> + Copy + Default> Value for T {
 
 #[derive(Debug)]
 pub struct Matrix<T: Value, const R: usize, const C: usize> {
-    data: [[T; R]; C],
+    values: [[T; C]; R],
 }
 
 impl<T: Value, const R: usize, const C: usize> Matrix<T, R, C> {
-    pub fn new(data: [[T; R]; C]) -> Self {
+    pub fn new(values: [[T; C]; R]) -> Self {
         Matrix {
-            data
+            values
         }
     }
 }
@@ -23,13 +23,43 @@ impl <T: Value, const R: usize, const C: usize> Add<T> for Matrix<T, R, C> {
     type Output = Matrix<T, R, C>;
 
     fn add(self, other: T) -> Matrix<T, R, C> {
-        let mut data = [[T::default(); R]; C];
+        let mut values = [[T::default(); C]; R];
 
         for col in 0..C {
             for row in 0..R {
-                data[col][row] = self.data[col][row] + other;            }
+                values[row][col] = self.values[row][col] + other;            }
         }
 
-        Matrix { data }
+        Matrix { values }
+    }
+}
+
+impl <T: Value, const R: usize, const C: usize> Mul<T> for Matrix<T, R, C> {
+    type Output = Matrix<T, R, C>;
+
+    fn mul(self, other: T) -> Matrix<T, R, C> {
+        let mut values = [[T::default(); C]; R];
+
+        for col in 0..C {
+            for row in 0..R {
+                values[row][col] = self.values[row][col] * other;            }
+        }
+
+        Matrix { values }
+    }
+}
+
+impl <T: Value, const R: usize, const C: usize> Add<Matrix<T, R, C>> for Matrix<T, R, C> {
+    type Output = Matrix<T, R, C>;
+
+    fn add(self, other: Matrix<T, R, C>) -> Matrix<T, R, C> {
+        let mut values = [[T::default(); C]; R];
+
+        for col in 0..C {
+            for row in 0..R {
+                values[row][col] = self.values[row][col] + other.values[row][col];            }
+        }
+
+        Matrix { values }
     }
 }
