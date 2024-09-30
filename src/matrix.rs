9f79997ebc3b1743710f8de695b72b092bd6fa0c
Ally <bagkingally@gmail.com>
2024-09-30 17:14:13 -0400

    Implement matrix multiplication

--- src/matrix.rs
+++ src/matrix.rs
@@ -17,17 +17,30 @@ impl<T: Value, const R: usize, const C: usize> Matrix<T, R, C> {
             values
         }
     }
-}
+
+    pub fn transpose(&self) -> Matrix<T, C, R> {
+        let mut values = [[T::default(); R]; C];
+
+        for col in 0..C {
+            for row in 0..R {
+                values[col][row] = self.values[row][col];
+            }
+        }
+
+        Matrix { values }
+    }
+ }
 
 impl <T: Value, const R: usize, const C: usize> Add<T> for Matrix<T, R, C> {
     type Output = Matrix<T, R, C>;
 
-    fn add(self, other: T) -> Matrix<T, R, C> {
+    fn add(self, other: T) -> Self::Output {
         let mut values = [[T::default(); C]; R];
 
         for col in 0..C {
             for row in 0..R {
-                values[row][col] = self.values[row][col] + other;            }
+                values[row][col] = self.values[row][col] + other;
+            }
         }
 
         Matrix { values }
@@ -37,7 +50,7 @@ impl <T: Value, const R: usize, const C: usize> Add<T> for Matrix<T, R, C> {
 impl <T: Value, const R: usize, const C: usize> Mul<T> for Matrix<T, R, C> {
     type Output = Matrix<T, R, C>;
 
-    fn mul(self, other: T) -> Matrix<T, R, C> {
+    fn mul(self, other: T) -> Self::Output {
         let mut values = [[T::default(); C]; R];
 
         for col in 0..C {
@@ -52,12 +65,30 @@ impl <T: Value, const R: usize, const C: usize> Mul<T> for Matrix<T, R, C> {
 impl <T: Value, const R: usize, const C: usize> Add<Matrix<T, R, C>> for Matrix<T, R, C> {
     type Output = Matrix<T, R, C>;
 
-    fn add(self, other: Matrix<T, R, C>) -> Matrix<T, R, C> {
+    fn add(self, other: Matrix<T, R, C>) -> Self::Output {
         let mut values = [[T::default(); C]; R];
 
         for col in 0..C {
             for row in 0..R {
                 values[row][col] = self.values[row][col] + other.values[row][col];            }
+        }
+
+        Matrix { values }
+    }
+}
+
+impl <T: Value, const R: usize, const C: usize, const K: usize> Mul<Matrix<T, R, K>> for Matrix<T, K, C> {
+    type Output = Matrix<T, R, C>;
+
+    fn mul(self, other: Matrix<T, R, K>) -> Self::Output {
+        let mut values = [[T::default(); C]; R];
+
+        for col in 0..C {
+            for row in 0..R {
+                for i in 0..K {
+                    values[col][row] = values[col][row] + self.values[row][i] + other.values[i][col];
+                }
+            }
         }
 
         Matrix { values }
