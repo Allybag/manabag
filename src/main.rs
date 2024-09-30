b7ff802a2fe101cb070382ac87bf7d6eb9c30a52
Ally <bagkingally@gmail.com>
2024-09-30 14:52:21 -0400

    Allow adding a scalar to a matrix

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ pub mod matrix;
 fn main() {
     let vec = matrix::Matrix::new([[1.0, 2.0, 3.0]]);
     let mat = matrix::Matrix::new([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]);
-    println!("Vector: {vec:?}, Matrix: {mat:?}");
+    let added = mat + 2.0;
+    println!("Vector: {vec:?}, Matrix: {added:?}");
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,9 +1,9 @@
 use std::ops::{Add, Mul};
 
-pub trait Value: Add<Output = Self> + Mul<Output = Self> + Copy {
+pub trait Value: Add<Output = Self> + Mul<Output = Self> + Copy + Default {
 }
 
-impl <T: Add<Output = T> + Mul<Output = T> + Copy> Value for T {
+impl <T: Add<Output = T> + Mul<Output = T> + Copy + Default> Value for T {
 }
 
 #[derive(Debug)]
@@ -16,5 +16,20 @@ impl<T: Value, const R: usize, const C: usize> Matrix<T, R, C> {
         Matrix {
             data
         }
+    }
+}
+
+impl <T: Value, const R: usize, const C: usize> Add<T> for Matrix<T, R, C> {
+    type Output = Matrix<T, R, C>;
+
+    fn add(self, other: T) -> Matrix<T, R, C> {
+        let mut data = [[T::default(); R]; C];
+
+        for col in 0..C {
+            for row in 0..R {
+                data[col][row] = self.data[col][row] + other;            }
+        }
+
+        Matrix { data }
     }
 }
